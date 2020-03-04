4ae37fe726efe14a276d548d4e4b3984dedf352b
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-03-04 23:57:05 +0000

    src(util): create Regex

---
+++ src/util/Regex.ts
@@ -0,0 +1,16 @@
+export default {
+  prefix(clientID: string, prefix: string) {
+    return new RegExp(
+      `^(<@!?${clientID}>|${prefix.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})\\s*`
+    );
+  },
+  mentionPrefix(clientID: string) {
+    return new RegExp(`^<@!?${clientID}>`);
+  },
+  id: /\d{17,19}/g,
+  user: {
+    tag: /.{2,32}(#)\d{4}/g,
+    username: /.{2,32}/g
+  },
+  emoji: /<(?::a)?:\w{2,32}:(\d{17,19})>/
+};
