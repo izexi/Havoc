e89a614812716d1789abb81a64c85a08249f0466
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-03-04 23:59:02 +0000

    feat(index): extend structures and handle message event

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import * as dotenv from 'dotenv';
 import HavoClient from './client/Havoc';
 import Logger from './util/Logger';
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import { Structures } from 'discord.js';
 
 dotenv.config();
 
-const Havoc = new HavoClient();
-
-Havoc.login(process.env.TOKEN);
-Havoc.on('ready', () => Logger.log('ready'));
+const extensionsDir = join(__dirname, 'structures', 'extensions');
+fs.readdir(extensionsDir).then(structures => {
+  structures.forEach(struct =>
+    Structures.extend(
+      struct.match(/Havoc([a-z]+).js/i)![1],
+      () => require(join(extensionsDir, struct)).default
+    )
+  );
+  const Havoc = new HavoClient();
+  Havoc.login(process.env.TOKEN);
+  Havoc.on('ready', () => Logger.log('ready2'));
+  Havoc.on('message', Havoc.commandHandler.handle);
+});
