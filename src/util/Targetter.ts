8ac7af9045d16f4db31a04be7e1238d3ca5a30a5
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-03-25 14:23:30 +0000

    ref(Targetter): properly type Targetter obj & export resolveTargetter

--- src/util/Targetter.ts
+++ src/util/Targetter.ts
@@ -1,9 +1,8 @@
+import { MessageMentions, GuildMember, User, Role, Emoji } from 'discord.js';
 import HavocMessage from '../structures/extensions/HavocMessage';
 import HavocGuild from '../structures/extensions/HavocGuild';
-import { MessageMentions, GuildMember } from 'discord.js';
 import Regex from './Regex';
 import Havoc from '../client/Havoc';
-import HavocUser from '../structures/extensions/HavocUser';
 
 export enum Target {
   USER = 'user',
@@ -12,7 +11,28 @@ export enum Target {
   EMOJI = 'emoji'
 }
 
-export const Targetter = {
+interface Targets {
+  user: User;
+  member: GuildMember;
+  role: Role;
+  emoji: Emoji;
+}
+
+type NotFound = null | undefined;
+
+const Targetter: {
+  [key in Target]?: {
+    mentionOrIDSearch(
+      query: string,
+      client: Havoc
+    ): Promise<Targets[key] | NotFound>;
+    nameSearch(
+      query: string,
+      guild: HavocGuild | null
+    ): Promise<Targets[key] | NotFound>;
+    get(arg: string, message: HavocMessage): Promise<Targets[key] | NotFound>;
+  };
+} = {
   [Target.USER]: {
     async mentionOrIDSearch(query: string, client: Havoc) {
       if (!query) return null;
@@ -28,7 +48,7 @@ export const Targetter = {
       const findFn = (member: GuildMember) =>
         member.user.tag === query || member.user.username === query;
       return (
-        guild.members.cache.find(findFn) ||
+        guild.members.cache.find(findFn)?.user ||
         guild.members
           .fetch()
           .then(
@@ -36,7 +56,7 @@ export const Targetter = {
               members.find(
                 member =>
                   findFn(member) || member.displayName.toLowerCase() === query
-              ) || null
+              )?.user
           )
           .catch(() => null) ||
         null
@@ -48,18 +68,17 @@ export const Targetter = {
         (await this.nameSearch(query, message.guild))
       );
     }
-  },
-
-  async parse(
-    message: HavocMessage
-  ): Promise<{ [Target.USER]?: HavocUser | null }> {
-    const parsed = {};
-    for (const { type } of message.command!.args!) {
-      Object.assign(parsed, {
-        // @ts-ignore
-        [type]: await Targetter[type].get(message.args[0], message)
-      });
-    }
-    return parsed;
   }
 };
+
+export async function resolveTarget(
+  message: HavocMessage,
+  parsed: { [key in Target]?: Targets[key] | NotFound } = {}
+) {
+  for (const { type } of message.command!.args!) {
+    Object.assign(parsed, {
+      [type]: await Targetter[type]!.get(message.args[0], message)
+    });
+  }
+  return parsed;
+}
