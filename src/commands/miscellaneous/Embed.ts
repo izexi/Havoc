d674d280ae3cf4e661538a9f5cc5ddbbc7fe65c8
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-03-27 18:42:35 +0000

    src(commands): create Embed

---
+++ src/commands/miscellaneous/Embed.ts
@@ -0,0 +1,20 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import { Target } from '../../util/Targetter';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description: 'Embeds the inputted text.',
+      args: {
+        type: Target.TEXT,
+        required: true,
+        prompt: 'enter the text that you would like to embed.'
+      }
+    });
+  }
+
+  async run({ message, text }: { message: HavocMessage; text: string }) {
+    message.respond({ setDescription: text });
+  }
+}
