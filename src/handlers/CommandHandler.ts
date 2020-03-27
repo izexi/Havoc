3e4eeddde20897fc243f8f8062914d213151ec3b
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-03-27 18:39:40 +0000

    feat(CommandHandler): use prompts to fill in required args

--- src/handlers/CommandHandler.ts
+++ src/handlers/CommandHandler.ts
@@ -2,7 +2,7 @@ import Logger from '../util/Logger';
 import Command from '../structures/bases/Command';
 import Util from '../util/Util';
 import HavocMessage from '../structures/extensions/HavocMessage';
-import { resolveTarget } from '../util/Targetter';
+import { Targetter, Targets } from '../util/Targetter';
 
 export default class {
   commands: Map<Command['name'], Command> = new Map();
@@ -36,10 +36,9 @@ export default class {
       !message.content.startsWith(message.prefix)
     )
       return;
-    const possibleCmd = message.args
-      .shift()
-      ?.substring(message.prefix.length)
-      ?.toLowerCase();
+    const possibleCmd = message.args[0]
+      .substring(message.prefix.length)
+      .toLowerCase();
     if (!possibleCmd) return;
     const command =
       this.commands.get(possibleCmd) ||
@@ -48,11 +47,24 @@ export default class {
       );
     if (!command) return;
     message.command = command;
-    const params = { message };
+    message.args.shift();
+    const params: {
+      message: HavocMessage;
+      [key: string]: HavocMessage | Targets[keyof Targets];
+    } = { message };
     if (command.args) {
-      await resolveTarget(message)
-        .then(parsed => Object.assign(params, parsed))
-        .catch(error => Logger.error('Targetter#parse()', error));
+      for (const { type, required, prompt, promptOpts } of command.args) {
+        let found = await Targetter[type]!.get(message, message.args[0]);
+        if (!found && required) {
+          const responses = await message.createPrompt({
+            initialMsg: promptOpts?.initial || prompt!,
+            invalidMsg: promptOpts?.invalid || '',
+            target: type
+          });
+          found = responses[type];
+        }
+        params[type] = found!;
+      }
     }
     command.run.call(message.client, params);
   };
