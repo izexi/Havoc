6d70ab72ba3f0b703236b21500cd61dbf533b19d
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-03-28 15:34:58 +0000

    src(commands): create Invite

---
+++ src/commands/miscellaneous/Invite.ts
@@ -0,0 +1,22 @@
+import Command from '../../structures/bases/Command';
+import Havoc from '../../client/Havoc';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description: 'View an invite link to invite me to a server.',
+      aliases: ['inv']
+    });
+  }
+
+  async run(this: Havoc, { message }: { message: HavocMessage }) {
+    message.respond(
+      `[Click here to invite **${
+        this.user!.username
+      }** to your server.](https://discordapp.com/oauth2/authorize?client_id=${
+        this.user!.id
+      }&scope=bot&permissions=2146958591)`
+    );
+  }
+}
