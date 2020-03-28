7fc4c083b62571d2599d18904579aadd84c807f5
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-03-28 22:12:19 +0000

    src(commands): create MemberList

---
+++ src/commands/miscellaneous/MemberList.ts
@@ -0,0 +1,38 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import Util from '../../util/Util';
+import { Target } from '../../util/Targetter';
+import { Role } from 'discord.js';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description: 'View a list of members on this server.',
+      aliases: ['ml', 'members', 'memberslist'],
+      args: {
+        required: true,
+        type: Target.ROLE,
+        prompt:
+          "mention the role / enter the role's name or ID that you would like to list members from."
+      }
+    });
+  }
+
+  async run({ message, role }: { message: HavocMessage; role: Role }) {
+    const members = await message
+      .guild!.members.fetch()
+      .then(() => role.members);
+    message.paginate({
+      title: `List of ${members.size} ${Util.plural(
+        'member',
+        members.size
+      )} that have the role \`${role.name}\``,
+      descriptions: members.map(
+        member =>
+          `• **${member.displayName}**\n  (${member.user.tag} - ${member.user.id})`
+      ),
+      maxPerPage: 20,
+      page: Number(message.arg)
+    });
+  }
+}
