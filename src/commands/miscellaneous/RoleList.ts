7ec6ff02490e2eecef339008fe956f253e27802c
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-03-29 11:56:13 +0100

    src(commands): create RoleList

---
+++ src/commands/miscellaneous/RoleList.ts
@@ -0,0 +1,30 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import Util from '../../util/Util';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description: 'View a list of roles on this server.',
+      aliases: ['rl', 'roles', 'roleslist']
+    });
+  }
+
+  async run({ message }: { message: HavocMessage }) {
+    const roles = message.guild?.roles.cache
+      .filter(({ id }) => id !== message.guild?.id)
+      .sort((prev, curr) => curr.position - prev.position)!;
+
+    message.paginate({
+      title: `List of ${roles.size} ${Util.plural('role', roles.size)} in ${
+        message.guild!.name
+      }`,
+      descriptions: roles.map(
+        role => `• **${role.name}**
+                  (${role} - ${role.id})`
+      ),
+      maxPerPage: 20,
+      page: Number(message.arg)
+    });
+  }
+}
