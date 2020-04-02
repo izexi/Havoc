2615352382e3a1ba3e9ad8e1849cf2042b199c56
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-02 17:10:42 +0100

    src(commands): create Clap

---
+++ src/commands/fun/Clap.ts
@@ -0,0 +1,23 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import { Target } from '../../util/Targetter';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description: 'Displays👏text👏like👏this👏',
+      args: {
+        type: Target.TEXT,
+        required: true,
+        prompt: 'enter👏the👏text👏that👏you👏would👏like👏to👏embed.'
+      }
+    });
+  }
+
+  async run({ message, text }: { message: HavocMessage; text: string }) {
+    message.send(text.replace(/ /g, '👏'), {
+      split: { char: '👏' },
+      disableMentions: 'everyone'
+    });
+  }
+}
