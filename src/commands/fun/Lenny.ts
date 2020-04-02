3f6aa0118444ddce05585fc9b8f0283a68ae96a7
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-02 17:10:59 +0100

    src(commands): create Lenny

---
+++ src/commands/fun/Lenny.ts
@@ -0,0 +1,15 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description: '( ͡° ͜ʖ ͡°)',
+      aliases: ['( ͡° ͜ʖ ͡°)']
+    });
+  }
+
+  async run({ message }: { message: HavocMessage }) {
+    message.respond('( ͡° ͜ʖ ͡°)', false, true);
+  }
+}
