ee0c45a11886a97988b5ad6e695ae16353c70049
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-02 17:11:09 +0100

    src(commands): create Omegalul

---
+++ src/commands/fun/Omegalul.ts
@@ -0,0 +1,25 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import { Target } from '../../util/Targetter';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description:
+        'Replaces any `o`s with <:OMEGALUL:695303479790665758> in the inputted message.',
+      args: {
+        type: Target.TEXT,
+        required: true,
+        prompt:
+          'enter the text that you would like to <:OMEGALUL:695303479790665758>.'
+      }
+    });
+  }
+
+  async run({ message, text }: { message: HavocMessage; text: string }) {
+    message.send(text.replace(/o/gi, '<:OMEGALUL:695303479790665758>'), {
+      split: { char: '👏' },
+      disableMentions: 'everyone'
+    });
+  }
+}
