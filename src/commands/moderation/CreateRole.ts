f2179083baedde23406c38db85c6af0fb2893d26
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-04 12:56:54 +0100

    src(commands): create CreateRole

---
+++ src/commands/moderation/CreateRole.ts
@@ -0,0 +1,29 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import { Target } from '../../util/Targetter';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description: 'Creates a role with the inputted text as the name.',
+      aliases: ['cr'],
+      args: {
+        type: Target.TEXT,
+        required: true,
+        prompt: 'enter the name that you would like to name the new role'
+      },
+      requiredPerms: 'MANAGE_ROLES'
+    });
+  }
+
+  async run({ message, text }: { message: HavocMessage; text: string }) {
+    message
+      .guild!.roles.create({
+        data: { name: text.substring(0, 100) },
+        reason: `Created By ${message.author.tag}`
+      })
+      .then(({ name }) =>
+        message.respond(`I have created a new role named \`${name}\`.`)
+      );
+  }
+}
