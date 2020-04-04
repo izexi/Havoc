928062887373f5e02017ca92a8c6c8e2fe16a29b
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-04 11:51:42 +0100

    src(commands): create Botclear

---
+++ src/commands/moderation/Botclear-config.ts
@@ -0,0 +1,89 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import Havoc from '../../client/Havoc';
+import GuildConfig from '../../structures/entities/GuildConfig';
+import { Target } from '../../util/Targetter';
+import { defaultPrefixes } from './Botclear';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description:
+        'Configure the `botclear` command (view, add or remove bot prefixes).',
+      sub: true,
+      args: {
+        required: true,
+        type: message => {
+          const possibleOption = message.arg?.toLowerCase();
+          if (!possibleOption) return;
+          if (['view', 'add', 'remove'].includes(possibleOption))
+            return message.shiftArg(possibleOption);
+        },
+        promptOpts: {
+          initial:
+            'what would you like to configure from prefixes that will be botcleared?\nEnter `view` / `add` / `remove`.',
+          invalid: 'You will need to enter either `view` or `add` or `remove`.'
+        }
+      },
+      requiredPerms: 'MANAGE_MESSAGES'
+    });
+  }
+
+  async run(
+    this: Havoc,
+    {
+      message,
+      fn: option
+    }: {
+      message: HavocMessage;
+      fn: 'view' | 'add' | 'remove';
+    }
+  ) {
+    const guildConfig = await this.db.find(GuildConfig, message.guild!.id);
+    const bcPrefixes = guildConfig
+      ? guildConfig.bcPrefixes || defaultPrefixes(message)
+      : defaultPrefixes(message);
+
+    if (option === 'view')
+      return message.respond(
+        `current messages that are prefixed with ${bcPrefixes!
+          .map(prefix => `\`${prefix}\``)
+          .join(', ')} will be cleared when the botclear command is used.`
+      );
+
+    const prefix =
+      message.arg ||
+      (
+        await message.createPrompt({
+          initialMsg: `**${message.author.tag}** enter the prefix that you would ${option} to the botclears.`,
+          target: Target.TEXT
+        })
+      ).text;
+    if (!prefix) return;
+
+    if (option === 'add') {
+      if (bcPrefixes!.includes(prefix)) {
+        return message.respond(
+          `\`${prefix}\` is already prefix that is being botcleared, you can view all the prefixes by entering \`${message.prefix}bc config view\`.`
+        );
+      }
+      bcPrefixes!.push(prefix);
+    } else {
+      if (!bcPrefixes!.includes(prefix)) {
+        return message.respond(
+          `\`${prefix}\` isn't a prefix that is being botcleared, you can view all the prefixes by entering \`${message.prefix}bc config view\`.`
+        );
+      }
+      bcPrefixes!.splice(bcPrefixes!.indexOf(prefix), 1);
+    }
+
+    await this.db.upsert(GuildConfig, message.guild!.id, { bcPrefixes });
+    message.respond(
+      `I have ${option}${
+        option === 'add' ? 'e' : ''
+      }d \`${prefix}\` to the list of prefixes that will be botcleared, you can view all the prefixes by entering \`${
+        message.prefix
+      }bc config view\`.`
+    );
+  }
+}
---
+++ src/commands/moderation/Botclear.ts
@@ -0,0 +1,95 @@
+import Command, { Status } from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import Havoc from '../../client/Havoc';
+import GuildConfig from '../../structures/entities/GuildConfig';
+import Util from '../../util/Util';
+
+export const defaultPrefixes = (message: HavocMessage) => [
+  message.prefix!,
+  '!',
+  '.',
+  '?'
+];
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description:
+        'Deletes recent bot messages & messages that contain bot commands (messages that start with popular bot prefixes along with the configured prefixes) to keep the chat clean.',
+      aliases: ['bc'],
+      args: {
+        type: message => {
+          const possibleSubCmd = message.arg?.toLowerCase();
+          if (!possibleSubCmd) return;
+          if (possibleSubCmd === 'config') {
+            message.command = message.client.commandHandler.commands.get(
+              'botclear-config'
+            )!;
+            message.runCommand();
+            return Status.SUBCOMMAND;
+          }
+          return null;
+        }
+      },
+      requiredPerms: 'MANAGE_MESSAGES'
+    });
+  }
+
+  async run(
+    this: Havoc,
+    {
+      message,
+      fn
+    }: {
+      message: HavocMessage;
+      fn: number | null;
+    }
+  ) {
+    if (fn === Status.SUBCOMMAND) return;
+    const emojis = [
+      '<:botclear1:486606839015014400>',
+      '<:botclear2:486606870618963968>',
+      '<:botclear3:486606906337525765>'
+    ];
+    const guildConfig = await this.db.find(GuildConfig, message.guild!.id);
+    const bcPrefixes = guildConfig
+      ? guildConfig.bcPrefixes || defaultPrefixes(message)
+      : defaultPrefixes(message);
+    const messages = await message.channel.messages
+      .fetch({ limit: 100 })
+      .catch(() => null);
+
+    if (!messages)
+      return message.respond(
+        'I encountered an error when attempting to fetch recent messages to botclear, maybe try again later?'
+      );
+
+    const cleared = await message.channel
+      .bulkDelete(
+        messages.filter(
+          msg =>
+            msg.author!.bot ||
+            (bcPrefixes || [message.prefix!, '!', '.', '?']).some(prefix =>
+              msg.content.startsWith(prefix)
+            )
+        ),
+        true
+      )
+      .catch(() => null);
+
+    if (!cleared)
+      return message.respond(
+        'I encountered an error when attempting to botclear the messages, maybe try again later?'
+      );
+
+    message
+      .respond(
+        `bot cleared \`${cleared.size} ${Util.plural(
+          'message',
+          cleared.size
+        )}\` ${emojis[Util.randomInt(0, emojis.length - 1)]}`
+      )
+      .then(async message => message.delete({ timeout: 1300 }))
+      .catch(() => null);
+  }
+}
