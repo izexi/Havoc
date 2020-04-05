e1ee84c0d6e13a0df9ffd5e24c7e338c75da4ed0
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-06 00:01:35 +0100

    feat(Havoc): add schedules prop and initialise scheduless

--- src/client/Havoc.ts
+++ src/client/Havoc.ts
@@ -2,18 +2,40 @@ import { Client } from 'discord.js';
 import Database from '../structures/Database';
 import Logger from '../util/Logger';
 import CommandHandler from '../handlers/CommandHandler';
+import MuteSchedule from '../schedules/Mute';
+import Schedule from '../structures/bases/Schedule';
+import { AnyEntity } from 'mikro-orm';
+import { once } from 'events';
+import Util from '../util/Util';
 
 export default class Havoc extends Client {
   db = new Database();
 
   commandHandler = new CommandHandler();
 
+  schedules: Map<string, Schedule<AnyEntity>> = new Map([
+    ['mute', new MuteSchedule(this)]
+  ]);
+
   constructor(options = {}) {
     super(options);
-    this.init().catch(error => Logger.error('Havoc#init()', error));
+    this.init();
   }
 
   async init() {
-    this.db.init().catch(error => Logger.error('Database#init()', error));
+    await this.db.init().catch(error => Logger.error('Database#init()', error));
+    await once(this, 'ready');
+    Promise.all(
+      [...this.schedules.values()].map(schedule => schedule.init())
+    ).then(
+      () =>
+        Logger.status(
+          `Initialised ${this.schedules.size} ${Util.plural(
+            'schedule',
+            this.schedules.size
+          )}`
+        ),
+      error => Logger.error('Schedule#init()', error)
+    );
   }
 }
