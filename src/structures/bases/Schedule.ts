7f5622a75c19bf394e1baace6dfd30788240f0b6
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-06 19:43:24 +0100

    feat(Schedule): add a tasks Map and (d/en)queue()

--- src/structures/bases/Schedule.ts
+++ src/structures/bases/Schedule.ts
@@ -1,10 +1,12 @@
 import Havoc from '../../client/Havoc';
 import HavocGuild from '../extensions/HavocGuild';
 import { EntityProps } from '../Database';
-import { AnyEntity } from 'mikro-orm';
+import { AnyEntity, Collection } from 'mikro-orm';
 
 export default abstract class Schedule<T extends AnyEntity> {
   client: Havoc;
+
+  tasks: Map<T['id'], NodeJS.Timer> = new Map();
 
   constructor(client: Havoc) {
     this.client = client;
@@ -14,13 +16,27 @@ export default abstract class Schedule<T extends AnyEntity> {
     return this.load();
   }
 
-  abstract add(guild: HavocGuild, mute: EntityProps<T>): Promise<void>;
+  enqueue(entity: T, delay: number) {
+    if (!this.tasks.has(entity.id))
+      this.tasks.set(
+        entity.id,
+        this.client.setTimeout(() => this.end(entity), delay)
+      );
+  }
+
+  async dequeue(entity: T, parent: Collection<T>) {
+    const task = this.tasks.get(entity.id);
+    if (task) {
+      this.client.clearTimeout(task);
+      this.tasks.delete(entity.id);
+    }
+    parent.remove(entity);
+    await this.client.db.flush();
+  }
+
+  abstract start(guild: HavocGuild, mute: EntityProps<T>): Promise<void>;
 
   abstract end(entity: T): Promise<void>;
-
-  abstract delete(entity: T): Promise<void>;
-
-  abstract enqueue(entity: T): void;
 
   abstract load(): Promise<void>;
 }
