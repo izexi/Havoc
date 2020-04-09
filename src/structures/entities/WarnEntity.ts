ac59a90ad33df403a7b32974b8d51bfc808065f7
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-09 11:45:16 +0100

    src(entities): create WarnEntity

---
+++ src/structures/entities/WarnEntity.ts
@@ -0,0 +1,24 @@
+import { Entity, Property, AnyEntity, PrimaryKey, ManyToOne } from 'mikro-orm';
+import { EntityProps } from '../Database';
+import GuildEntity from './GuildEntity';
+
+export interface Warn {
+  reason?: string;
+  warner: string;
+}
+
+@Entity()
+export default class WarnEntity implements AnyEntity {
+  @PrimaryKey()
+  id!: string;
+
+  @Property()
+  history!: Warn[];
+
+  @ManyToOne()
+  guild!: GuildEntity;
+
+  constructor(data: EntityProps<WarnEntity> = {}) {
+    Object.assign(this, data);
+  }
+}
