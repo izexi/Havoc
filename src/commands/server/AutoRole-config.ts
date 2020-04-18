37cb302c7c77dbd732d1c897a74e486df80c978e
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-18 12:55:33 +0100

    src(commands): create AutoRole

---
+++ src/commands/server/AutoRole-config.ts
@@ -0,0 +1,47 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import { Target } from '../../util/Targetter';
+import GuildEntity from '../../structures/entities/GuildEntity';
+import { Role } from 'discord.js';
+import Havoc from '../../client/Havoc';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description:
+        'Configure the autorole (role to add to members as they join) for this server.',
+      aliases: ['autorole-enable'],
+      args: {
+        type: Target.ROLE,
+        required: true,
+        prompt:
+          'mention the role, or enter the ID of the role that would like to set as the autorol.'
+      },
+      sub: true,
+      requiredPerms: 'MANAGE_GUILD'
+    });
+  }
+
+  async run(
+    this: Havoc,
+    { message, role }: { message: HavocMessage; role: Role }
+  ) {
+    const existing = await this.db
+      .find(GuildEntity, message.guild!.id)
+      .then(guild =>
+        guild?.autorole ? message.guild!.roles.cache.get(guild.autorole) : null
+      );
+
+    await this.db.upsert(GuildEntity, message.guild!.id, {
+      autorole: role.id
+    });
+
+    message.respond(
+      `${
+        existing
+          ? `I have updated the autorole from ${existing} to ${role}`
+          : `I have enabled autorole for ${role} for this server.`
+      }`
+    );
+  }
+}
---
+++ src/commands/server/AutoRole-disable.ts
@@ -0,0 +1,27 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import GuildEntity from '../../structures/entities/GuildEntity';
+import Havoc from '../../client/Havoc';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description:
+        'Disable the autorole (role to add to members as they join) for this server.',
+      sub: true,
+      requiredPerms: 'MANAGE_GUILD'
+    });
+  }
+
+  async run(this: Havoc, { message }: { message: HavocMessage }) {
+    const guildEntity = await this.db.find(GuildEntity, message.guild!.id);
+
+    if (!guildEntity || !guildEntity.autorole)
+      return message.respond(`autorole has not been enabled on this server.`);
+
+    delete guildEntity.autorole;
+    await this.db.flush();
+
+    message.respond(`I have disabled autorole for this server.`);
+  }
+}
