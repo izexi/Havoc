1a34e201d2f8e59f6d70d197b2d5299904bbc109
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-18 12:51:10 +0100

    feat(Havoc): cache commonly accessed custom guild props

--- src/client/Havoc.ts
+++ src/client/Havoc.ts
@@ -6,8 +6,11 @@ import MuteSchedule from '../schedules/Mute';
 import GiveawaySchedule from '../schedules/Giveaway';
 import { once } from 'events';
 import Util from '../util/Util';
+import HavocGuild from '../structures/extensions/HavocGuild';
 
 export default class Havoc extends Client {
+  initialised = false;
+
   db = new Database();
 
   commandHandler = new CommandHandler();
@@ -27,7 +30,7 @@ export default class Havoc extends Client {
     await once(this, 'ready');
 
     const schedules = Object.values(this.schedules);
-    Promise.all(schedules.map(schedule => schedule.init())).then(
+    await Promise.all(schedules.map(schedule => schedule.init())).then(
       () =>
         Logger.status(
           `Initialised ${schedules.length} ${Util.plural(
@@ -37,5 +40,32 @@ export default class Havoc extends Client {
         ),
       error => Logger.error('Schedule#init()', error)
     );
+
+    const guilds = await this.db.guildRepo.findAll({ populate: ['tags'] });
+    await Promise.all(
+      guilds.map(async guildEntity => {
+        const guild = this.guilds.cache.get(guildEntity.id) as HavocGuild;
+        if (!guild) {
+          if (
+            guildEntity.deletedAt &&
+            guildEntity.deletedAt > new Date(Date.now() + 12096e5)
+          )
+            return this.db.guildRepo.removeAndFlush(guildEntity);
+        }
+
+        if (guildEntity.logs) guild.logs = guildEntity.logs.webhook;
+        if (guildEntity.prefix) guild.prefix = guildEntity.prefix;
+        if (guildEntity.bcPrefixes) guild.bcPrefixes = guildEntity.bcPrefixes;
+        if (guildEntity.welcomer) guild.welcomer = guildEntity.welcomer;
+        if (guildEntity.tags && guildEntity.tags.count()) {
+          await guildEntity.tags.init();
+          guildEntity.tags
+            .getItems()
+            .forEach(({ name, content }) => guild.tags.set(name, content));
+        }
+      })
+    );
+
+    this.initialised = true;
   }
 }
