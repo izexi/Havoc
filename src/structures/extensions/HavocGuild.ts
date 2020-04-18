9b4dbaf1c6b60f63be30cb61e72607fbc942ab40
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-18 12:50:43 +0100

    feat(HavocGuild): add tags, bcPrefixes, logs, welcomer & logHook getter

--- src/structures/extensions/HavocGuild.ts
+++ src/structures/extensions/HavocGuild.ts
@@ -1,5 +1,20 @@
-import { Guild } from 'discord.js';
+import { Guild, WebhookClient } from 'discord.js';
 
 export default class extends Guild {
   prefix = process.env.PREFIX!;
+
+  tags = new Map();
+
+  bcPrefixes = ['!', '.', '?'];
+
+  logs?: {
+    id: string;
+    token: string;
+  };
+
+  welcomer?: string;
+
+  get logHook() {
+    return this.logs ? new WebhookClient(this.logs.id, this.logs.token) : null;
+  }
 }
