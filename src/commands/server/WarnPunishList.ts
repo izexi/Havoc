d07b4db311c1b1a88b8dd84f1a6401abc934fab9
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-18 12:56:15 +0100

    src(commands): create WarnPunishList

---
+++ src/commands/server/WarnPunishList.ts
@@ -0,0 +1,38 @@
+import Command from '../../structures/bases/Command';
+import Havoc from '../../client/Havoc';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import { getPunishments } from '../moderation/Warn';
+import Util from '../../util/Util';
+import ms = require('ms');
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description: 'View the current punishments for warns on the server.',
+      aliases: ['warnplist', 'punishmentlist'],
+      requiredPerms: 'MANAGE_GUILD'
+    });
+  }
+
+  async run(this: Havoc, { message }: { message: HavocMessage }) {
+    const punishments = await getPunishments(this, message);
+
+    message.respond({
+      setDescription: Util.codeblock(
+        [...punishments.entries()]
+          .sort(([prev], [curr]) => prev - curr)
+          .map(([amount, punishment]) => {
+            const [action, time] = punishment.split(' ');
+            return `${amount} => ${
+              action === 'mute'
+                ? `Mute for ${ms(Number(time), {
+                    long: true
+                  })}`
+                : Util.captialise(punishment)
+            }\n`;
+          })
+          .join('\n')
+      )
+    });
+  }
+}
