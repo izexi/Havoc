1f01b935ada4c9c02b690eedc20dd0f689dfb670
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-19 23:20:20 +0100

    src(commands): create Donate

---
+++ src/commands/miscellaneous/Donate.ts
@@ -0,0 +1,18 @@
+import Command from '../../structures/bases/Command';
+import Havoc from '../../client/Havoc';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description: 'Supports me.'
+    });
+  }
+
+  async run(this: Havoc, { message }: { message: HavocMessage }) {
+    message.respond({
+      setDescription: `Donate to me by clicking on [Patreon](https://www.patreon.com/user?u=15028160 or [PayPal](https://paypal.me/havoceditor) (join [this server](https://discord.gg/3Fewsxq) to receive the Donator role)
+				              All donations will be used towards hosting costs for the bot. Donations are NOT required or expected, but are much appreciated`
+    });
+  }
+}
