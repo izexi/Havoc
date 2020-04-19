024d95ef363afbf6e507802ce6a9cbfb8dcd9a8d
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-19 23:20:16 +0100

    src(commands): create WidePeepoHappy

---
+++ src/commands/fun/WidePeepoHappy.ts
@@ -0,0 +1,22 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      description:
+        '<:widePeepoHappy1:678636233131425805><:widePeepoHappy2:678636283119140864><:widePeepoHappy3:678636320406634516><:widePeepoHappy4:678636358335463445>',
+      aliases: [
+        '<:widePeepoHappy1:678636233131425805><:widePeepoHappy2:678636283119140864><:widePeepoHappy3:678636320406634516><:widePeepoHappy4:678636358335463445>'
+      ]
+    });
+  }
+
+  async run({ message }: { message: HavocMessage }) {
+    message.respond(
+      '<:widePeepoHappy1:678636233131425805><:widePeepoHappy2:678636283119140864><:widePeepoHappy3:678636320406634516><:widePeepoHappy4:678636358335463445>',
+      false,
+      true
+    );
+  }
+}
