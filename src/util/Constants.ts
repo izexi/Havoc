b4974f65108ada6a6692cdfb8dbd40c648421b7e
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-21 09:54:07 +0100

    src(util): create Constants

---
+++ src/util/Constants.ts
@@ -0,0 +1,9 @@
+// TODO: Move all magic numbers and random strings here
+
+export enum STATUS_ICONS {
+  READY = 'https://www.dictionary.com/e/wp-content/uploads/2016/01/paris-green-color-paint-code-swatch-chart-rgb-html-hex.png',
+  DISCONNECTED = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Red.svg/240px-Red.svg.png',
+  ERROR = 'https://www.publicdomainpictures.net/pictures/30000/velka/dark-red-background.jpg',
+  RECONNECTING = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/ICS_Quebec.svg/1200px-ICS_Quebec.svg.png',
+  RESUMED = 'https://www.tileflair.co.uk/images/sized/images/products/carnaby-yellow/101115/Carnaby-Yellow-180x120mm-RTCRY6_d1974737e8129c7bb88892b7409c39bb.jpg'
+}
