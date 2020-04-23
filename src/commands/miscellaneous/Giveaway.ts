e3ba72508e849ad16256aae342392d00c405e59a
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-23 10:28:50 +0100

    ref: use Command#subParent for parent subcommands TODO: Refactor the implementation for this (subParent#subCommand?)

--- src/commands/miscellaneous/Giveaway.ts
+++ src/commands/miscellaneous/Giveaway.ts
@@ -1,28 +1,14 @@
-import Command, { Status } from '../../structures/bases/Command';
+import Command from '../../structures/bases/Command';
 
 export default class extends Command {
   constructor() {
     super(__filename, {
       description: 'Creates a giveaway with options.',
       aliases: ['g'],
-      args: {
-        required: true,
-        type: message => {
-          const possibleSubCmd = message.arg?.toLowerCase();
-          if (!possibleSubCmd) return;
-          if (['start', 'end', 'reroll', 'config'].includes(possibleSubCmd)) {
-            message.command = message.client.commandHandler.find(
-              `giveaway-${possibleSubCmd}`
-            )!;
-            message.runCommand();
-            return Status.SUBCOMMAND;
-          }
-        },
-        promptOpts: {
-          initial:
-            'would you like to `start`, `end`, `reroll` or `config` a giveaway? (enter the according option)',
-          invalid: 'You will need to enter either `start`, `end` or `reroll`'
-        }
+      subParent: {
+        subCommands: ['start', 'end', 'reroll', 'config'],
+        prompt:
+          'would you like to `start`, `end`, `reroll` or `config` a giveaway? (enter the according option)'
       },
       requiredPerms: 'MANAGE_GUILD'
     });
--- src/commands/server/Logs.ts
+++ src/commands/server/Logs.ts
@@ -1,28 +1,13 @@
-import Command, { Status } from '../../structures/bases/Command';
+import Command from '../../structures/bases/Command';
 
 export default class extends Command {
   constructor() {
     super(__filename, {
       description: 'Configure the logs for the server.',
-      args: {
-        required: true,
-        type: message => {
-          const possibleSubCmd = message.arg?.toLowerCase();
-          if (!possibleSubCmd) return;
-          if (['enable', 'config', 'disable'].includes(possibleSubCmd)) {
-            message.command = message.client.commandHandler.find(
-              `logs-${possibleSubCmd}`
-            )!;
-            message.runCommand();
-            return Status.SUBCOMMAND;
-          }
-        },
-        promptOpts: {
-          initial:
-            'would you like to `enable`, `config` or `disable` the logs for this server? (enter the according option)',
-          invalid:
-            'You will need to enter either `enable`, `config` or `disable`'
-        }
+      subParent: {
+        subCommands: ['enable', 'config', 'disable'],
+        prompt:
+          'would you like to `enable`, `config` or `disable` the logs for this server? (enter the according option)'
       },
       requiredPerms: 'MANAGE_GUILD'
     });
