302ac641b05df45c5037b571051be61b8354c74c
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-28 13:59:22 +0100

    feat(HavocGuild): add modlogs & sendModLog()

--- src/structures/extensions/HavocGuild.ts
+++ src/structures/extensions/HavocGuild.ts
@@ -2,15 +2,21 @@ import {
   Guild,
   WebhookClient,
   MessageEmbed,
-  DiscordAPIError
+  DiscordAPIError,
+  GuildMember,
+  User
 } from 'discord.js';
-import { EmbedMethods } from './HavocMessage';
+import HavocMessage, { EmbedMethods } from './HavocMessage';
 import Util from '../../util';
 import HavocTextChannel from './HavocTextChannel';
 import GuildEntity from '../entities/GuildEntity';
 import Havoc from '../../client/Havoc';
+import ms = require('ms');
+import { MODLOGS_COLOUR } from '../../util/CONSTANTS';
 
 export default class extends Guild {
+  client!: Havoc;
+
   prefix = process.env.PREFIX!;
 
   tags = new Map();
@@ -25,6 +31,8 @@ export default class extends Guild {
       token: string;
     };
   };
+
+  modlogs?: string;
 
   autorole?: string;
 
@@ -83,5 +91,57 @@ export default class extends Guild {
       }
       delete this.logs;
     });
+  }
+
+  async sendModLog({
+    message,
+    target,
+    reason,
+    duration
+  }: {
+    message: HavocMessage;
+    target: GuildMember | User;
+    reason?: string;
+    duration?: number;
+  }) {
+    if (!this.modlogs) return;
+
+    if (!(target instanceof User))
+      target = await this.client.users.fetch(target.id);
+
+    const channel = this.client.channels.cache.get(
+      this.modlogs
+    ) as HavocTextChannel;
+    if (!channel) {
+      delete this.modlogs;
+      const guildEntity = await this.client.db.find(GuildEntity, this.id);
+
+      if (guildEntity && guildEntity.modlogs) {
+        delete guildEntity.modlogs;
+        await this.client.db.flush();
+      }
+      return;
+    }
+
+    const fields = [
+      { name: '❯Member', value: target.tag },
+      {
+        name: '❯Action',
+        value: Util.captialise(
+          message.command.name.replace('clearwarnings', 'Clear Warnings')
+        )
+      }
+    ];
+    if (duration)
+      fields.push({ name: '❯Duration', value: ms(duration, { long: true }) });
+    if (reason) fields.push({ name: '❯Reason', value: reason });
+
+    channel.send(
+      new MessageEmbed()
+        .setAuthor(message.author.tag, message.author.pfp)
+        .addFields(fields)
+        .setColor(MODLOGS_COLOUR[message.command.name])
+        .setTimestamp()
+    );
   }
 }
