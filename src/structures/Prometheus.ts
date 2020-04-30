c5b88eabfb764fa68db81f7f1246823307a6059d
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-30 14:53:58 +0100

    feat(Prometheus): add more counters and gauges

--- src/structures/Prometheus.ts
+++ src/structures/Prometheus.ts
@@ -2,6 +2,7 @@ import { Counter, Gauge, register } from 'prom-client';
 import { createServer } from 'http';
 import { parse } from 'url';
 import Havoc from '../client/Havoc';
+import { cpu, drive } from 'node-os-utils';
 import {
   SECONDS,
   MEGABYTES,
@@ -12,32 +13,59 @@ import {
 export default class {
   client: Havoc;
 
-  started = false;
+  cpuUsage = { user: 0, system: 0 };
+
+  commandCounter = this.createCounter(
+    'havoc_command_counter',
+    'Counter of processed commands',
+    ['command_name']
+  );
+
+  eventCounter = this.createCounter(
+    'havoc_event_counter',
+    'Counter of recieved events',
+    ['event_name']
+  );
+
+  guildGauge = this.createGauge(
+    'havoc_guild_gauge',
+    'Gauge for total guild count'
+  );
+
+  cpuGauge = this.createGauge('havoc_cpu_gauge', 'Gauge for CPU percentage');
+
+  memoryGauge = this.createGauge(
+    'havoc_memory_gauge',
+    'Gauge for memory being used'
+  );
+
+  driveGauge = this.createGauge(
+    'havoc_drive_gauge',
+    'Gauge for drive being used'
+  );
+
+  register = register;
 
   constructor(client: Havoc) {
     this.client = client;
     this.start();
   }
 
-  prometheus = {
-    messagesCounter: new Counter({
-      name: 'havoc_message_counter',
-      help: 'Counter of recieved messages'
-    }),
-    memoryGauge: new Gauge({
-      name: 'havoc_memory_gauge',
-      help: 'Gauge for memory being used'
-    }),
-    register
-  };
+  createCounter(name: string, help: string, labelNames?: string[]) {
+    return new Counter({ name, help, labelNames });
+  }
+
+  createGauge(name: string, help: string) {
+    return new Gauge({ name, help });
+  }
 
   start() {
     createServer((req, res) => {
       if (req.url && parse(req.url).pathname === '/metrics') {
         res.writeHead(HTTP_OK, {
-          'Content-Type': this.prometheus.register.contentType
+          'Content-Type': this.register.contentType
         });
-        res.write(this.prometheus.register.metrics());
+        res.write(this.register.metrics());
       }
       res.end();
     })
@@ -48,12 +76,18 @@ export default class {
         });
 
         setInterval(
-          () =>
-            this.prometheus.memoryGauge.set(
-              MEGABYTES(process.memoryUsage().heapUsed)
-            ),
+          () => this.memoryGauge.set(MEGABYTES(process.memoryUsage().heapUsed)),
           SECONDS(1)
         ).unref();
+
+        setInterval(() => {
+          cpu
+            .usage()
+            .then(percentage => this.cpuGauge.set(Math.round(percentage)));
+          drive
+            .info('/')
+            .then(({ usedGb }) => this.driveGauge.set(Math.round(usedGb)));
+        }, SECONDS(5)).unref();
       });
   }
 }
