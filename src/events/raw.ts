ae1ea3624a7dd6617ff2eb159fcd5298c37c1724
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-04-30 14:54:27 +0100

    src(events): create raw

---
+++ src/events/raw.ts
@@ -0,0 +1,8 @@
+import Havoc from '../client/Havoc';
+
+export default function(
+  this: Havoc,
+  { op: opCode, t: eventName }: { op: number; t: string }
+) {
+  this.prometheus.eventCounter.inc({ event_name: eventName ?? opCode });
+}
