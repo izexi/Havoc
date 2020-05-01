b42d13ca37b4643d412a42cf372fff452fdcc5ea
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-05-01 11:30:27 +0100

    feat(message): add fixed responses

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,6 +1,17 @@
 import HavocMessage from '../structures/extensions/HavocMessage';
 import Havoc from '../client/Havoc';
+import regex from '../util/regex';
 
 export default function(this: Havoc, message: HavocMessage) {
+  if (message.content.toLowerCase() === ',havoc')
+    return message.channel.send('hi');
+
+  const prefix = message.guild?.prefix ?? process.env.PREFIX;
+  if (this.user && regex.mentionPrefix(this.user.id).test(message.content))
+    return message.respond(
+      `my prefix here is \`${prefix}\` (you can also mention me).
+      You can view a list of all my commands by doing \`${prefix}help\``
+    );
+
   this.commandHandler.handle(message);
 }
