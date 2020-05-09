1feddf18c838f57615adb0a8796bc0e70d05f8e9
izexi <43889168+izexi@users.noreply.github.com>
2020-05-09 22:14:47 +0100

    misc(Donate): fix hyperlink

--- src/commands/miscellaneous/Donate.ts
+++ src/commands/miscellaneous/Donate.ts
@@ -12,7 +12,7 @@ export default class extends Command {
 
   async run(this: Havoc, { message }: { message: HavocMessage }) {
     message.respond({
-      setDescription: `Donate to me by clicking on [Patreon](https://www.patreon.com/user?u=15028160 or [PayPal](https://paypal.me/havoceditor) (join [this server](https://discord.gg/3Fewsxq) to receive the Donator role)
+      setDescription: `Donate to me by clicking on [Patreon](https://www.patreon.com/user?u=15028160) or [PayPal](https://paypal.me/havoceditor) (join [this server](https://discord.gg/3Fewsxq) to receive the Donator role)
 				              All donations will be used towards hosting costs for the bot. Donations are NOT required or expected, but are much appreciated`
     });
   }
