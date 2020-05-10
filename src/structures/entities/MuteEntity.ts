a720a94c551dd7cc7ec613fdb047b4c147104931
izexi <43889168+izexi@users.noreply.github.com>
2020-05-10 21:22:05 +0100

    ref(MuteEntity): use uuid.v4() for the id

--- src/structures/entities/MuteEntity.ts
+++ src/structures/entities/MuteEntity.ts
@@ -1,11 +1,12 @@
 import { Entity, Property, AnyEntity, PrimaryKey, ManyToOne } from 'mikro-orm';
 import { EntityProps } from '../Database';
 import GuildEntity from './GuildEntity';
+import { v4 } from 'uuid';
 
 @Entity()
 export default class MuteEntity implements AnyEntity {
   @PrimaryKey()
-  id!: number;
+  id = v4();
 
   @Property()
   start = new Date();
