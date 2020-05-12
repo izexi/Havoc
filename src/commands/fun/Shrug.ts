97420e82eae7db7f63f9eb4d852a2de4a33d324c
izexi <43889168+izexi@users.noreply.github.com>
2020-05-12 13:54:51 +0100

    src(commands): create Shrug

---
+++ src/commands/fun/Shrug.ts
@@ -0,0 +1,20 @@
+import Command from '../../structures/bases/Command';
+import HavocMessage from '../../structures/extensions/HavocMessage';
+import { EMOJIS } from '../../util/CONSTANTS';
+
+export default class extends Command {
+  constructor() {
+    super(__filename, {
+      dm: true,
+      description: EMOJIS.SHRUG,
+      aliases: [EMOJIS.SHRUG]
+    });
+  }
+
+  async run({ message }: { message: HavocMessage }) {
+    message.respond(EMOJIS.SHRUG, {
+      author: false,
+      contentOnly: true
+    });
+  }
+}
